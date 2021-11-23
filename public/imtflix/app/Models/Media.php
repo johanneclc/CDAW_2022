9ab0dfffb9d66510e96a303b6d52e9edbcf92cd2
johanneclc <johannecalcoen@gmail.com>
2021-11-23 11:00:30 +0000

    Crud Media

--- public/imtflix/app/Models/Media.php
+++ public/imtflix/app/Models/Media.php
@@ -10,16 +10,17 @@ class Media extends Model
     use HasFactory;
 
     protected $table = 'medias';
+    protected $primaryKey = 'id_media';
 
     protected $guarded = [
         'created_at','update_at'
     ];
-    public function getCategoriesOfMedias()
+    public function categories()
     {
-        return $this->hasMany(Categorie::class);
+        return $this->hasManyThrough(Categorie::class, CategorieMedia::class,'id_media','id_categorie');
     }
 
-    public function getTypeOfMedia(){
-        return $this->hasOne(Type::class);
+    public function type(){
+        return $this->hasOne(Type::class,'id_type','id_type');
     }
 }

6feba90ee7d6c4e265fea44d22fe1869ff26b39d
wifeko <49137001+wifeko@users.noreply.github.com>
2021-11-28 16:21:39 +0100

    final commit profil LAST UPDATE

--- public/imtflix/resources/views/mon_profil.blade.php
+++ public/imtflix/resources/views/mon_profil.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
 <section class="py-5">
+<form action="{{ route('users.update', Auth::user()->id) }}" method="POST" enctype="multipart/form-data"> 
+        @csrf
+        @method('PUT')
+     
 			  <div class="container my-5">
 				  <div class="row justify-content-center">
 					  <div class="col-lg-6">
@@ -21,22 +25,31 @@
 								<div class="hero-text-tablecell">
 									<div class="single-testimonial-slider">
 										<div class="client-avater">
-                                        <img src="assets/img/avaters/avatar3.png" alt="">
-                                        <img src="/{{ Auth::user()->chemin_avatar }}" width="500px">
+                                        <img src="/chemin_avatar/{{ Auth::user()->chemin_avatar }}" width="500px">
 										</div>
 										<div class="client-meta">
 											<h3 class="text-white">Pseudo <span class="text-white">{{ Auth::user()->name }}</span></h3>
-											<span id="mail">{{ Auth::user()->email }}</span>
-											
+                                            <input type="text" name="name" value="{{ Auth::user()->name }}" class="form-control" placeholder="Name">
+
+                                            <span id="mail">{{ Auth::user()->email }}</span>
+                                            <input type="text" name="email" value="{{ Auth::user()->email }}" class="form-control" placeholder="Email">
+
 											<div class="last-icon">
 												<i class="fas fa-quote-right"></i>
 											</div>
                                             
-											<div class="hero-btns">
-												<a href="modifier_profil" class="boxed-btn">Modifier leee profil</a>
-                                                <div class="pull-right">
-                                                    <a class="btn btn-primary" href="listeMedias"> Back</a>
+                                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                                <div class="form-group">
+                                                    <strong>Image:</strong>
+                                                    <input type="file" name="chemin_avatar" class="form-control" placeholder="chemin_avatar">
+                                                    <img src="/chemin_avatar/{{ Auth::user()->chemin_avatar }}" width="300px">
                                                 </div>
+                                            </div>
+											<div class="hero-btns">
+												<a href="listeMedias" class="boxed-btn">Retour</a>
+                                                <button type="submit" class="boxed-btn">Modifier Profile</button>
+
+                                                
 											</div>
 										</div>
 									</div>
@@ -50,34 +63,7 @@
 			  </div>
 
 
-        <form action="{{ route('users.update', Auth::user()->id) }}" method="POST" enctype="multipart/form-data"> 
-        @csrf
-        @method('PUT')
-     
-         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Name:</strong>
-                    <input type="text" name="name" value="{{ Auth::user()->name }}" class="form-control" placeholder="Name">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>email:</strong>
-                    <textarea class="form-control" style="height:150px" name="email" placeholder="Email">{{ Auth::user()->email }}</textarea>
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Image:</strong>
-                    <input type="file" name="chemin_avatar" class="form-control" placeholder="chemin_avatar">
-                    <img src="/chemin_avatar/{{ Auth::user()->chemin_avatar }}" width="300px">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </div>
+       
      
     </form>
 

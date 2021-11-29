6293048d678a8d781d4651ca8877105995c6ab22
johanneclc <johannecalcoen@gmail.com>
2021-11-29 14:40:23 +0000

    seeder medias

--- public/imtflix/resources/views/templateAccueil.blade.php
+++ public/imtflix/resources/views/templateAccueil.blade.php
@@ -62,17 +62,20 @@
 
 								<li ><a href="films">Film</a>
 									<ul class="sub-menu">
-										<li><a href="film.html">Film</a></li>
+                                        {{-- @foreach($categories as $categories)
+                                            <li><a href="films/{{$categorie->id_categorie}}">$categorie->name</a></li>
+                                        @endforeach --}}
+										<li><a href="films">Film</a></li>
 									</ul>
 								</li>
 								<li ><a href="serie.html">Série</a>
 									<ul class="sub-menu">
-										<li><a href="serie.html">Serie</a></li>
+										<li><a href="series">Serie</a></li>
 									</ul>
 								</li>
-								<li ><a href="manga.html">Manga</a>
+								<li ><a href="manga.html">Animés</a>
 									<ul class="sub-menu">
-										<li><a href="manga.html">Manga</a></li>
+										<li><a href="animes">Animés</a></li>
 									</ul>
 								</li>
 								<li><a href="dessinanime.html">Dessin Animé</a>
